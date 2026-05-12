6ae875944307deee67ff78a8f7bbe55c180906d9
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-05-12 17:27:29 +0700

    kode program

--- agresi.cpp
+++ agresi.cpp
@@ -4,3 +4,28 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
+int main(){ //agregasi
+    ibu* varIbu = new inu("dini");
+    ibu* varibu2 = new ibu("novi");
+    anak* varanak1 = new anak("tono");
+    anak* varanak2 = new anak("rini");
+    anak* varanak3 = new anak ("dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu1->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    system("pause");
+
+    return 0;
+}
