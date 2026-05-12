b5adb775bb8114620e10412377939b1c0ca1fba3
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-05-12 17:15:24 +0700

    membuat public

--- agresiIbu.cpp
+++ agresiIbu.cpp
@@ -3,3 +3,32 @@
 #include <vector>
 
 class ibu {
+public: 
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "ibu \"" << nama << "\" ada\n";
+
+    }
+    ~ibu() {
+         cout << "ibu \"" << nama << "\" tidak ada\n";
+
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak(){
+ cout << "Dfatar Anak dari ibu \"" << this->nama <<"\":\n";
+    /*for (auto& a : daftar_anak) {
+     cout << a->nama << "\n";
+    }*/
+   for (int i = 0; i< daftar_anak.size(); i++) {
+    cout << daftar_Anak[i]->nama << endl;
+   }
+   cout << endl;
+}
+#endif
