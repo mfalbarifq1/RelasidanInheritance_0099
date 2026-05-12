6f3a50ac0defd49ac25d4cbda20a57035386e827
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-05-12 17:37:14 +0700

    kode paradigma

---
+++ paradigmaoop1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi class mahasiswa
+class Mahasiswa {
+    //membuat member class private
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+    //membuat member class public
+public:
+    //membuat member initialization list dari class mahasiswa
+    Mahasiswa(int pId, string pNama, float pNilai)
+        : nama(pNama), nilai(pNilai), id(pId) {
+
+    };
+
+    //membuat destructor dari class mahasiswa
+    ~Mahasiswa() {
+        cout << "ID : " << id << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+};
+
+int main()
+{
+    //deklarasi dan pemberian nilai untuk objek mhs
+    Mahasiswa mhs(12, "Asrori", 90.5);
+
+    return 0;
+}
+
